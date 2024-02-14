5381f7d0a8b2122581d3124721d9a7dfa31f44d0
EmelieCharlotte <160015366+EmelieCharlotte@users.noreply.github.com>
2024-02-14 13:29:45 +0100

    Delete routes.py

--- routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Blueprint
-
-home = Blueprint('home',__name__)
-
-@home.route('/')
-@home.route('/home')
-def home_page():
-    return render_template('home.html', title = 'home')     
